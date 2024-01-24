5c6d48c3155ff54905623b448a93c80c68d81e4c
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-01-24 23:10:04 +0530

    Add files via upload

---
+++ Even_Odd.cpp
@@ -0,0 +1,18 @@
+// //Write a program to check a number is even or odd
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int x;
+//     cin >> x;
+
+//     if((x&1) == 0){
+//         cout << "Even";
+//     }
+//     else{
+//         cout << "Odd";
+//     }
+
+// }
+
+//write a program to swap two numbers without using third variable
