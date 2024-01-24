f57f200e7df6baedc01ed5182632a7715624aaa4
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-01-24 23:11:00 +0530

    Add files via upload

---
+++ Swapping.cpp
@@ -0,0 +1,20 @@
+// Write a progrma to swap two numbers without using third varaible 
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a;
+//     int b;
+
+//     cin >> a >> b;
+
+//     cout << "Before swapping" << a  << " " << b << endl;
+//     a= a^b;
+//     b= a^b;
+//     a= a^b;
+
+//     cout << "After swapping" << a << " " << b;
+// }
+
+//write a program to find the bit of any number of ith position
