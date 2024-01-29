2d07b223e9a6a80b1b48e266e0043a6a1b04bafc
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-01-29 20:04:24 +0530

    Add files via upload

---
+++ chk_number.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+
+// int chk_number(int number){
+//     if(number>0){
+//         cout << "Positive";
+//     }
+//     else if(number<0){
+//         cout << "Negative";
+//     }
+//     else if(number == 0){
+//         cout << "Neutral";
+//     }
+// }
+// int main(){
+//     int n;
+//     cout << "Enter the number : " << endl;
+//     cin >> n;
+
+//     chk_number(n);
+
+
+// }
