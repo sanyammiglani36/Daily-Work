d6b25628fac61be5834104f7afaca2ec692d1897
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-01-29 20:03:45 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout << "Enter the number : " ;
+//     cin >> n;
+//     int arm = 0;
+//     int temp = n;
+
+//     while(n>0){
+//         int r = n%10;
+//         arm = (r*r*r) + arm;
+//         n = n/10;
+//     }
+//     if(temp == arm){
+//         cout << "Armstrong Number" << n << endl;
+//     }
+//     else{
+//         cout << "Not Armstrong";
+//     }
+// }
