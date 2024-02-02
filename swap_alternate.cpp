d6237fc48b0860d5a6aa1928ebba40ffef03c115
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-02-02 19:23:33 +0530

    Add files via upload

---
+++ swap_alternate.cpp
@@ -0,0 +1,61 @@
+// #include<iostream>
+// using namespace std;
+
+// void print_array(int arr[],int n){
+//     for(int i = 0;i<n;i++){
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// void swap_alternate(int arr[],int size){
+//     for(int i = 0;i<size;i= i +2){
+//         if(i+1 < size){
+//             swap(arr[i],arr[i+1]);
+//         }
+//     }
+// }
+// int main(){
+
+    
+//     int even[8] = {1,2,3,4,5,6,7,8};
+//     int odd[5] = {2,3,4,5,6};
+
+//     swap_alternate(even,8);
+//     print_array(even,8);
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// void print_array(int arr[],int n){
+//     for(int i = 0;i<n;i++){
+//         cout << arr[i] << " ";
+//     }
+// }
+// void swap_alternate(int arr[],int size){
+//     for(int i=0;i<size;i = i+2){
+//         if(i+1<size){
+//             swap(arr[i],arr[i+1]);
+//         }
+//     }
+// }
+// int main(){
+//     int even[8] = {1,2,3,4,5,6,7,8};
+//     int odd[5] = {2,3,4,5,6};
+
+//     swap_alternate(odd,5);
+//     print_array(odd,5);
+
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i= 0;i<n;i++){
+        cin >> arr[i];
+        
+    }
+}
