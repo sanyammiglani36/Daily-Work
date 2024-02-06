ff9c8c94022a0988a8cb46754830f51157e03a09
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-02-06 23:51:03 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << sizeof(n);
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float n;
+//     cout << sizeof(n);
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char n;
+//     cout << sizeof(n);
+// }
