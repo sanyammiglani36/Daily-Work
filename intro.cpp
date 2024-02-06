f794dfd9707612c960272ff41ca7e4ea09ecafa2
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-02-07 00:31:13 +0530

    Add files via upload

---
+++ intro.cpp
@@ -0,0 +1,14 @@
+// Linked list is a just a type of linear data structures and basically it is a collection of nodes.
+
+// nodes are the combination of data and address of next node
+
+// Advantage of linked list
+// it has dynamic data structure
+// no memeory wastage
+// insertion/deletion easy / no shifting there 
+
+//types of linked list
+// 1 - singly linked list
+// 2 - doubly linked list
+// 3 - circular linked list
+// 4 - circular doubly linked list
