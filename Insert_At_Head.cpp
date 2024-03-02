5ce9990ffe1a013fc3ef33feca0c01304aa0c013
Sanyam Miglani <122440583+sanyammiglani36@users.noreply.github.com>
2024-03-03 00:16:03 +0530

    Add files via upload

---
+++ Insert_At_Head.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    //Constructor
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head,int d){
+    //new Node Created
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+// void insertAtTail(Node* &tail,int d){
+//     Node* temp = new Node(d);
+//     tail-> next = temp;
+//     tail = tail -> next; 
+// }
+
+
+
+void print(Node* &head){
+    Node* temp =  head;
+
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp-> next; 
+    }
+    cout << endl;
+}
+
+
+
+int main(){
+    Node* node1 = new Node(10);
+
+    //cout << node1 -> data << endl;
+    //cout << node1 -> next << endl;
+
+    //head pointed to node1
+    Node* head = node1;
+    print(head);
+    insertAtHead(head,12);
+    print(head);
+
+    insertAtHead(head,15);
+    print(head);
+
+
+
+}
+
+
+
+
